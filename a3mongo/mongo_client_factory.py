060aa9b02fe6d6f529e49c24b50292a757ecb6eb
three-kinds <3179158552@qq.com>
2025-03-25 08:22:08 +0800

    Format code.

--- a3mongo/mongo_client_factory.py
+++ a3mongo/mongo_client_factory.py
@@ -3,7 +3,7 @@ from typing import Dict
 from pymongo import MongoClient
 from pymongo.database import Database
 
-DEFAULT_NAME = 'default'
+DEFAULT_NAME = "default"
 
 
 class MongoClientFactory:
@@ -14,7 +14,7 @@ class MongoClientFactory:
     def init_mongo_clients(cls, conf: dict):
         for name, cf in conf.items():
             client = MongoClient(**cf)
-            db = client[cf['authSource']]
+            db = client[cf["authSource"]]
 
             cls._name2conf[name] = cf
             cls._name2db[name] = db
--- a3mongo/mongo_table.py
+++ a3mongo/mongo_table.py
@@ -48,19 +48,21 @@ class MongoTable:
     def find(self, filter_dict: dict = None, sort_list: list = None, offset: int = None, limit: int = None) -> Cursor:
         params = dict()
         if filter_dict is not None:
-            params['filter'] = filter_dict
+            params["filter"] = filter_dict
 
         if sort_list is not None:
-            params['sort'] = sort_list
+            params["sort"] = sort_list
 
         if offset is not None:
-            params['skip'] = offset
+            params["skip"] = offset
         if limit is not None:
-            params['limit'] = limit
+            params["limit"] = limit
 
         return self._table.find(**params)
 
-    def find_with_pagination(self, filter_dict: dict = None, sort_list: list = None, page_size: int = 10000) -> Iterable[Dict]:
+    def find_with_pagination(
+        self, filter_dict: dict = None, sort_list: list = None, page_size: int = 10000
+    ) -> Iterable[Dict]:
         total_count = self._table.count_documents(filter=filter_dict)
         total_times = math.ceil(total_count / page_size)
 
@@ -71,7 +73,7 @@ class MongoTable:
                 yield entry
 
     def upsert_one(self, entry: dict, unique_field: str = None) -> (bool, UpdateResult | str):
-        unique_field = unique_field or '_id'
+        unique_field = unique_field or "_id"
         try:
             update_result = self._table.replace_one({unique_field: entry[unique_field]}, entry, upsert=True)
             return True, update_result
@@ -80,13 +82,11 @@ class MongoTable:
 
     # insert or update
     def upsert_many(self, entry_list: list, unique_field: str = None) -> (int, BulkWriteResult):
-        unique_field = unique_field or '_id'
+        unique_field = unique_field or "_id"
         request_list = list()
 
         for entry in entry_list:
-            request_list.append(
-                ReplaceOne({unique_field: entry[unique_field]}, entry, upsert=True)
-            )
+            request_list.append(ReplaceOne({unique_field: entry[unique_field]}, entry, upsert=True))
 
         error_count = 0
         while True:
@@ -95,8 +95,8 @@ class MongoTable:
                 return error_count, write_result
             except BulkWriteError as e:
                 error_index_list = list()
-                for error in e.details['writeErrors']:
-                    error_index_list.append(error['index'])
+                for error in e.details["writeErrors"]:
+                    error_index_list.append(error["index"])
 
                 error_count += len(error_index_list)
                 for index in sorted(error_index_list, reverse=True):
@@ -104,19 +104,11 @@ class MongoTable:
 
     # only insert not update
     def insert_many(self, entry_list: list, unique_field: str = None) -> (int, BulkWriteResult):
-        unique_field = unique_field or '_id'
+        unique_field = unique_field or "_id"
         request_list = list()
 
         for entry in entry_list:
-            request_list.append(
-                UpdateOne(
-                    {unique_field: entry[unique_field]},
-                    {
-                        "$setOnInsert": entry
-                    },
-                    upsert=True
-                )
-            )
+            request_list.append(UpdateOne({unique_field: entry[unique_field]}, {"$setOnInsert": entry}, upsert=True))
 
         error_count = 0
         while True:
@@ -125,8 +117,8 @@ class MongoTable:
                 return error_count, write_result
             except BulkWriteError as e:
                 error_index_list = list()
-                for error in e.details['writeErrors']:
-                    error_index_list.append(error['index'])
+                for error in e.details["writeErrors"]:
+                    error_index_list.append(error["index"])
 
                 error_count += len(error_index_list)
                 for index in sorted(error_index_list, reverse=True):
@@ -135,39 +127,38 @@ class MongoTable:
     def create_index_list(self, field_name_list: List[str], is_unique: bool = False):
         all_index = self._table.index_information()
         for field_name in field_name_list:
-            if f'{field_name}_1' in all_index:
+            if f"{field_name}_1" in all_index:
                 continue
             self._table.create_index(field_name, unique=is_unique, background=True)
 
     def drop_index_list(self, field_name_list: List[str]):
         all_index = self._table.index_information()
         for field_name in field_name_list:
-            index_name = f'{field_name}_1'
+            index_name = f"{field_name}_1"
             if index_name not in all_index:
                 continue
             self._table.drop_index(index_name)
 
     def set_validator(self, required_field_list: list, fields_validator: dict):
-        return self.db.command({
-            "collMod": self.table_name,
-            "validator": {
-                '$jsonSchema': {
-                    "bsonType": "object",
-                    "required": required_field_list,
-                    "properties": fields_validator
-                }
-            },
-            "validationLevel": "moderate"
-        })
+        return self.db.command(
+            {
+                "collMod": self.table_name,
+                "validator": {
+                    "$jsonSchema": {
+                        "bsonType": "object",
+                        "required": required_field_list,
+                        "properties": fields_validator,
+                    }
+                },
+                "validationLevel": "moderate",
+            }
+        )
 
     def disable_validator(self):
-        batch = self.db.command({'listCollections': 1, 'filter': {'name': self.table_name}})['cursor']['firstBatch']
+        batch = self.db.command({"listCollections": 1, "filter": {"name": self.table_name}})["cursor"]["firstBatch"]
         if len(batch) == 0:
             return
 
-        options = batch[0].get('options')
-        if isinstance(options, dict) and options.get('validator') is not None:
-            return self.db.command({
-                "collMod": self.table_name,
-                "validationLevel": "off"
-            })
+        options = batch[0].get("options")
+        if isinstance(options, dict) and options.get("validator") is not None:
+            return self.db.command({"collMod": self.table_name, "validationLevel": "off"})
